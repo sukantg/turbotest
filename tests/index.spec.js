209cf05873a093e30a56e7a6cc327a836f510963
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air.local>
2024-09-24 12:31:29 +0100

    update

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,6 @@
 const { test, expect } = require('@playwright/test');
+const moment = require('moment');
+let pagination = 2, currentTime = 0, previousTime = 0
 
 test('Stack Overflow', async ({ page }) => {
   // Navigate to Stack Overflow's questions page and wait for page to load
@@ -19,44 +21,67 @@ test('Stack Overflow', async ({ page }) => {
   const applyButton = await page.locator('button[data-se-uql-target="applyButton"]');
   await applyButton.click();
 
-// Create an array to store the extracted data
-const questionData = [];
+  const paginationButton = await page.locator('a[title="Show 50 items per page"]')
+  await paginationButton.click()
 
-  // Extract the question title
-  const titleElement = await page.locator('.s-post-summary--content-title a').first();
-  const title = await titleElement.innerText();
+  // Create an array to store the extracted data
+  const questionData = [];
 
-   // Extract tags
-   const tagListContainer = await page.locator('.js-post-tag-list-wrapper').first();
-   const listItems = await tagListContainer.locator('li');
+  do {
+    for (let i = 0; i < 50; i++) {
+    
+      // Extract the question title
+      const titleElement = await page.locator('.s-post-summary--content-title >> nth=' + i);
+      const title = await titleElement.innerText();
+
+      // Extract tags
+      const tagListContainer = await page.locator('.js-post-tag-list-wrapper >> nth=' + i);
+      const listItems = await tagListContainer.locator('li');
  
-   const tags = [];
-   for (const item of await listItems.elementHandles()) {
-     const tagText = await item.textContent();
-     tags.push(tagText.trim()); 
-   }
-   // Vetify that tags contain javascript or else test fails
-   expect(tags).toContain('javascript');
+      const tags = [];
+      for (const item of await listItems.elementHandles()) {
+        const tagText = await item.textContent();
+        tags.push(tagText.trim()); 
+      }
+
+      // Verify that tags contain javascript or else test fails
+      expect(tags).toContain('javascript');
   
 
-  // Extract the vote count
-  const voteCountElement = await page.locator('.s-post-summary--stats-item-number').first();
-  const voteCount = await voteCountElement.innerText();
+      // Extract the vote count
+      const voteCountElement = await page.locator('.s-post-summary--stats-item-number >> nth=' + i);
+      const voteCount = await voteCountElement.innerText();
 
 
-  // Extract the timestamp
-  const timestampElement = await page.locator('.s-user-card--time span').first();
-  const timestamp = await timestampElement.innerText();
+      // Extract the timestamp
+      const timestampElement = await page.locator('.s-user-card--time span >> nth=' + i);
+      const timestamp = await timestampElement.innerText();
+      // const momentObj = moment(timestamp);
+      // console.log(momentObj);
+      // currentTime = timestamp;
+      // // Verify that timestamp of the previous question is earlier than current one, or else test fails
+      // expect(previousTime).toBeLessThan(currentTime);
+      // previousTime = currentTime;
+
+      // Add the extracted data to the array of objects
+      questionData.push({
+      title,
+      tags,
+      voteCount,
+      timestamp
+      });
+
+    // console.log(questionData);
+  }
+    
+    const currentUrl = page.url();
+    const newUrl = currentUrl + '&page=' + pagination;
+    await page.goto(newUrl);
+    pagination++; 
   
+  } while (pagination <= 3);
 
-  // Add the extracted data to the array
-  questionData.push({
-  title,
-  tags,
-  voteCount,
-  timestamp
-});
- 
-console.log("Question data:", questionData);
+  console.log(questionData.length);
+
 });
 
